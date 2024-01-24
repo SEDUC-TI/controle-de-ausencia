959ea99b773a10a5c90f2ebef6f66be50768d531
Thales <thalis.mraz@gmail.com>
2024-01-24 14:55:04 -0300

    :sparkles: config tailwind

---
+++ src/rotas/Rotas.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes, /* Navigate */ } from 'react-router-dom';
+import Login from '../pages/Login';
+// import { useIsAuthenticated, useAuthUser } from 'react-auth-kit';
+
+const Rotas = () => {
+	return(
+		<Router>
+			<Routes>
+				<Route path='/' element={ <Login /> } />
+			</Routes>
+		</Router>
+	);
+};
+
+export default Rotas;
