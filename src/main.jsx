44ad0fffdd647e02ca14ff4ae46ed3821937f391
Thales Santos <thalis.mraz@gmail.com>
2024-01-23 23:20:46 -0300

    :sparkles: Initial Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+// import './index.scss';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from 'react-auth-kit';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <AuthProvider
+      authType="cookie"
+      authName="_auth"
+      cookieDomain={window.location.hostname}
+      cookieSecure={false}
+    >
+      <App />
+    </AuthProvider>
+  </BrowserRouter>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
