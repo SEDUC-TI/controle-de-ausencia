cfea5a82b661a4cfbb8dcec38c781812991d2910
Thales <thalis.mraz@gmail.com>
2024-01-24 16:28:09 -0300

    :sparkles: prettier config

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Login: React.FC = () => {
-	return (
-		<h1 className="text-3xl font-bold underline">
-            Hello world!
-		</h1>
-	);
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
 export default Login;
