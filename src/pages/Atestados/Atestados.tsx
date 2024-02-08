e4fe04d3024bc5f38fba75b0c8472011b7f068a8
Welson <53789831+welsondeivid@users.noreply.github.com>
2024-02-08 13:41:52 -0300

    ✨ feat: Criado Header para tela de servidores

--- src/pages/Atestados/Atestados.tsx
+++ src/pages/Atestados/Atestados.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import Header from '../../components/header/Header';
-import arrow from '../../assets/icons/arrow_back_curve.svg'
-
+import Header_servi
+ from '../../components/header/Header_servi';
 const Atestados: React.FC = () => {
 
     return(
         <div className='container max-w-full w-screen h-screen flex flex-col items-center'>
             <Header name="Thales"/>
 
-            <div className="container flex w-[1400px] h-[40px] bg-orange">
-                
-                <div className="container flex w-[121px] h-[28px] space-x-2">
-                    <div>
-                        {arrow}
-                    </div>
-                    <p className='text-concrete text-[28px]'>
-                        Voltar
-                    </p>
-                </div>
-            </div>
+            <Header_servi/>
+
         </div>
     );
 };
